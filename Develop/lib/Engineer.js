cc8b748370afe18907ad6b71e199862b1049f4be
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-04-13 18:52:03 -0500

    passed 8 of 14 tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require ("./Employee");
+
+class Engineer extends Employee {
+    constructor (GitHubUser) {
+        super(name, id, email);
+        this.GitHubUser = GitHubUser;
+    };
+
+    getRole() {
+        const role = "Engineer";
+        return role;
+    }''
+
+    getGithub() {
+        const GitHubUser = this.GitHubUser;
+        console.log(`github username is ${GitHubUser}`);
+    };
+};
+
+module.exports = Engineer;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require ("./Employee");
+
+class Intern extends Employee {
+    constructor (school) {
+        super(name, id, email);
+        this.school = school;
+    };
+
+    getRole() {
+        const role = "Intern";
+        return role;
+    };
+
+    getSchool() {
+        const school = this.school;
+        console.log(`Intern's school is ${school}`);
+    };
+};
+
+module.exports = Intern;
